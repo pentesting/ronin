c08100f1d36fef503c138ea89e061035471b1fab
postmodern <postmodern.mod3@gmail.com>
2010-03-21 17:43:11 -0700

    Added basic CSS, jQuery and a sub-apps menu.

---
+++ static/ronin/ui/web/public/js/page.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#sub-apps-menu > div.bottom").click(function() {
+    $("#sub-apps-menu > ul").slideToggle("fast");
+  });
+
+  $("#sub-apps-menu > ul").click(function() {
+    $(this).slideUp("fast");
+  });
+});
