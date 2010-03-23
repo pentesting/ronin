5b4eb7a6d6c494edcc1b4df9f530dc72c7bbe577
postmodern <postmodern.mod3@gmail.com>
2010-03-23 04:51:00 -0700

    Built-out the Ronin::UI::Web::Apps::Root app.
    * Added routes for managing the Database and Overlays.
    * Added views for all routes.
    * Added flash messages for successful and unsuccessful actions.
    * Added a Raphael.js powered intro which runs when the Root app is
      first visited.
    * Added the Ronin logo graphic.
    * Added a diamond graphic.

--- lib/ronin/ui/web/apps/root.rb
+++ lib/ronin/ui/web/apps/root.rb
@@ -20,6 +20,8 @@
 
 require 'ronin/ui/web/base'
 require 'ronin/ui/web/apps'
+require 'ronin/installation'
+require 'ronin/platform'
 require 'ronin/database'
 require 'ronin/config'
 
@@ -30,22 +32,179 @@ module Ronin
         class Root < Base
 
           self.lib_root = File.join(File.dirname(__FILE__),'..','..','..','..','..')
+          self.menu = {
+            'Database' => '/database',
+            'Overlays' => '/overlays',
+            'About' => '/about'
+          }
 
-          set :intro, true
-          set :intro_completed, false
+          set :intro, false
+          set :skip_intro, false
+
+          error Platform::OverlayNotFound do
+            flash[:error] = request.env['sinatra.error'].message
+
+            redirect '/overlays'
+          end
+
+          error Platform::DuplicateOverlay do
+            flash[:error] = request.env['sinatra.error'].message
+
+            redirect '/overlays'
+          end
 
           get '/' do
-            if (Root.intro && !(Root.intro_completed))
-              @intro = true
+            unless (Root.skip_intro || Root.intro)
+              Root.intro = true
+
+              redirect '/intro'
             end
 
-            result = erb :index
+            erb :index
+          end
 
-            if @intro
-              Root.intro_completed = true
+          get '/intro' do
+            erb :intro, :layout => false
+          end
+
+          get '/database' do
+            @repositories = Database.repositories
+
+            erb :database
+          end
+
+          get '/database/new' do
+            erb :database_new
+          end
+
+          post '/database/add' do
+            if params[:name].blank?
+              flash[:error] = "No Database Repository name given."
+              redirect '/database/add'
             end
 
-            result
+            if params[:uri].blank?
+              flash[:error] = "No Database Repository URI given."
+              redirect '/database/add'
+            end
+
+            name = params[:name].to_sym
+
+            Database.save do
+              Database.repositories[name] = params[:uri]
+            end
+
+            flash[:notice] = "Database repository #{params[:name]} added."
+            redirect '/database'
+          end
+
+          post '/database/:name/set' do
+            Database.save do
+              Database.repositories[name] = params[:uri]
+            end
+
+            flash[:notice] = "Database repository #{params[:name]} was set to #{params[:uri]}."
+            redirect '/database'
+          end
+
+          post '/database/:name/remove' do
+            name = params[:name].to_sym
+
+            Database.save do
+              Database.repositories.delete(name)
+            end
+
+            flash[:notice] = "Database repository #{params[:name]} was removed."
+            redirect '/database'
+          end
+
+          get '/overlays' do
+            @overlays = Platform::Overlay.all
+
+            erb :overlays
+          end
+
+          get '/overlays/:name/:domain' do
+            @overlay = Platform::Overlay.get(
+              "#{params[:name]}/#{params[:domain]}"
+            )
+
+            erb :overlay
+          end
+
+          get '/overlays/add' do
+            erb :overlays_add
+          end
+
+          post '/overlays/add' do
+            if params[:path].blank?
+              flash[:error] = "Must specify a path to add."
+              redirect '/overlays/add'
+            end
+
+            overlay = Platform::Overlay.add!(
+              :path => params[:path],
+              :uri => params[:uri],
+              :scm => params[:scm]
+            )
+
+            flash[:notice] = "Overlay #{overlay} was successfully added."
+            redirect '/overlays'
+          end
+
+          get '/overlays/install' do
+            erb :overlays_install
+          end
+
+          post '/overlays/install' do
+            if params[:uri].blank?
+              flash[:error] = "Must specify a URI to install from."
+              redirect '/overlays/add'
+            end
+
+            overlay = Platform::Overlay.install!(
+              :uri => params[:uri],
+              :scm => params[:scm]
+            )
+
+            flash[:notice] = "Overlay #{overaly} was successfilly installed."
+            redirect '/overlays'
+          end
+
+          get '/overlays/:name/:domain/uninstall' do
+            @overlay = Platform::Overlay.get(
+              "#{params[:name]}/#{params[:domain]}"
+            )
+
+            erb :overlays_uninstall
+          end
+
+          post '/overlays/uninstall' do
+            if params[:name].blank?
+              flash[:error] = "The 'name' of the Overlay to uninstall was not given"
+              redirect '/overlays'
+            end
+
+            if params[:domain].blank?
+              flash[:error] = "The 'domain' of the Overlay to uninstall was not given"
+              redirect '/overlays'
+            end
+
+            overlay = Platform::Overlay.uninstall!(
+              "#{params[:name]}/#{params[:domain]}"
+            )
+
+            flash[:notice] = "Overlay #{overlay} was successfully uninstalled."
+            redirect '/overlays'
+          end
+
+          get '/console' do
+            flash[:notice] = "Console not yet implemented."
+            redirect '/'
+          end
+
+          get '/about' do
+            erb :about
           end
 
         end
